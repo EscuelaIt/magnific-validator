3a0e0ed78e69ed41eab5af947f47011d21a2ed82
midesweb <malvarez@desarrolloweb.com>
2025-06-19 18:25:57 +0200

    add required rule

--- src/MagnificValidator.php
+++ src/MagnificValidator.php
@@ -4,16 +4,33 @@ namespace Escuelait\MagnificValidator;
 
 class MagnificValidator {
 
+  private array $errors;
+
   public function validateInput($input, array $rules = []) {
+    $this->errors = [];
     foreach($rules as $rule) {
       if($rule == 'email') {
-        return $this->validateEmail($input);
+        $this->validateEmail($input);
+      } elseif($rule == 'required') {
+        $this->validateRequired($input);
       }
+    }
+    if(count($this->errors) > 0) {
+      return false;
     }
     return true;
   }
 
   private function validateEmail($input) {
-    return filter_var($input, FILTER_VALIDATE_EMAIL) !== false;
+    if(filter_var($input, FILTER_VALIDATE_EMAIL) === false) {
+      $this->errors[] = 'The input should be an email';
+    }
   }
+
+  private function validateRequired($input) {
+    if(is_null($input) || $input == '') {
+      $this->errors[] = 'The input is required';
+    }
+  }
+
 }
--- tests/MagnificValidatorTest.php
+++ tests/MagnificValidatorTest.php
@@ -28,4 +28,39 @@ class MagnificValidatorTest extends TestCase {
     $result = $validator->validateInput('@escuela.it', ['email']);
     $this->assertFalse($result);
   }
+
+  #[Test]
+  public function itValidatesRequiredInput() {
+    $validator =  new MagnificValidator();
+    $result = $validator->validateInput('EscuelaIT', ['required']);
+    $this->assertTrue($result);
+  }
+
+  #[Test]
+  public function itValidatesRequiredInputIsNotProvided() {
+    $validator =  new MagnificValidator();
+    $result = $validator->validateInput('', ['required']);
+    $this->assertFalse($result);
+  }
+
+  #[Test]
+  public function itValidatesRequiredAndEmailInput() {
+    $validator =  new MagnificValidator();
+    $result = $validator->validateInput('alvaro@escuela.it', ['email', 'required']);
+    $this->assertTrue($result);
+  }
+
+  #[Test]
+  public function itValidatesIncorrectRequiredAndEmailInput() {
+    $validator =  new MagnificValidator();
+    $result = $validator->validateInput('no_es_un_email', ['required', 'email']);
+    $this->assertFalse($result);
+  }
+
+  #[Test]
+  public function itValidatesIncorrectEmailAndRequiredInput() {
+    $validator =  new MagnificValidator();
+    $result = $validator->validateInput('no_es_un_email', ['email', 'required']);
+    $this->assertFalse($result);
+  }
 }
